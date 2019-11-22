11d97dfee24cd0976498a5210dd49e6ea8b6895b
Hayden Menge <haydenmenge@ufl.edu>
2019-11-22 13:46:09 -0500

    commit

--- src/components/Console.js
+++ src/components/Console.js
@@ -4,8 +4,8 @@ import '../css/bootstrap.min.css';
 const Console = (props) => {
 
     return(
-        <div className="row">
-            <div className="col-md-6 m-auto">
+        <div className="row ">
+            <div className="col-md-6 m-auto ">
                 <form action="/upload" method="POST" enctype="multipart/form-data">
                     <div className="custom-file mb-3">
                         <input type='file' name='fileA' id='fileArt'>
@@ -13,7 +13,7 @@ const Console = (props) => {
                         <label htmlFor='fileArt' className='custom-file-label'>Choose Art</label>
                     </div>
                     <div className="custom-file mb-3">
-                        <input type='file' name='fileP' id='filePreview'>
+                        <input type='file' name='filePre' id='filePreview'>
                         </input>
                         <label htmlFor='filePreview' className='custom-file-label'>Choose Preview</label>
                     </div>
@@ -27,7 +27,13 @@ const Console = (props) => {
                         </input>
                         <label htmlFor='fileStems' className='custom-file-label'>Choose Stems</label>
                     </div>
-                    <div>
+                    <div className="custom-file mb-3">
+                        <input type="text" name="fileTitle" id="fileT"/>
+                        <label htmlFor='fileT' >Title</label>
+                    </div>
+                    <div className="custom-file mb-3">
+                        <input type="number" name="filePrice" id="fileP"/>
+                        <label htmlFor='fileT' >Price ($)</label>
                     </div>
                     <input type="submit" value="submit" className="btn btn-primary btn-block"/>
                 </form>
